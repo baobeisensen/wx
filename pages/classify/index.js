63865e4682bbdf9aadb388ae9ceaa951edfc71b5
zms00@139.com <zms00@139.com>
2019-12-13 18:03:11 +0800

    123

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -6,34 +6,32 @@ Page({
         curNav: 1,
 		curIndex: 0
     },
-    onLoad: function() {
+    // onLoad: function() {
+    //     var that = this
+    //     wx.request({
+    //         url: 'http://huanqiuxiaozhen.com/wemall/goodstype/typebrandList',
+    //         method: 'GET',
+    //         data: {},
+    //         header: {
+    //             'Accept': 'application/json'
+    //         },
+    //         success: function(res) {
+    //             that.setData({
+    //                 navLeftItems: res.data,
+    //                 navRightItems: res.data
+    //             })
+    //         }
+    //     })
+    // },
 
-        var that = this
-        
-        wx.request({
-            url: 'http://huanqiuxiaozhen.com/wemall/goodstype/typebrandList',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                that.setData({
-                    navLeftItems: res.data,
-                    navRightItems: res.data
-                })
-            }
-        })
-    },
-
-    //事件处理函数
-    switchRightTab: function(e) {
-        let id = e.target.dataset.id,
-			index = parseInt(e.target.dataset.index);
-		this.setData({
-			curNav: id,
-			curIndex: index
-		})
-    }
+    // //事件处理函数
+    // switchRightTab: function(e) {
+    //     let id = e.target.dataset.id,
+		// 	index = parseInt(e.target.dataset.index);
+		// this.setData({
+		// 	curNav: id,
+		// 	curIndex: index
+		// })
+    // }
 
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,9 @@ Page({
         autoplay: true,
         interval: 3000,
         duration: 1000,
-        loadingHidden: true  // loading
+        red: '#228B22',
+        loadingHidden: true,  // loading
+        images: ['https://gd1.alicdn.com/imgextra/i1/3709723567/O1CN01SjxPjN1cDjLGv1OeD_!!3709723567.jpg_400x400.jpg', 'https://gd1.alicdn.com/imgextra/i1/3709723567/O1CN0145XEGf1cDjLMvSBam_!!3709723567.jpg_400x400.jpg', 'https://gd2.alicdn.com/imgextra/i2/3709723567/O1CN01fQ3IQo1cDjLMieC8P_!!3709723567.jpg_400x400.jpg']
     },
 
     //事件处理函数
@@ -15,73 +17,74 @@ Page({
         //console.log(e.detail.current)
     },
 
-    onLoad: function() {
-        console.log('onLoad')
-        var that = this
-            //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
-        })
+    // onLoad: function() {
+    //     console.log('onLoad')
+    //     var that = this
+    //         //调用应用实例的方法获取全局数据
+    //     app.getUserInfo(function(userInfo) {
+    //         //更新数据
+    //         that.setData({
+    //             userInfo: userInfo
+    //         })
+    //     })
 
-        //sliderList 
-        wx.request({
-          url: 'http://zms123.51vip.biz/test/test',
-            method: 'GET',
-            // data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-              console.log(res)
-                that.setData({
-                    images: res.data
-                })
-              loadingHidden: false;
-            }
-        })
+    //     //sliderList 
+    //     // wx.request({
+    //     //   url: 'http://zms123.51vip.biz/test/test',
+    //     //     method: 'GET',
+    //     //     // data: {},
+    //     //     header: {
+    //     //         'Accept': 'application/json'
+    //     //     },
+    //     //     success: function(res) {
+    //     //       console.log(res)
+    //     //         that.setData({
+    //      //           images: res.data
+    //     //         })
+    //     //       loadingHidden: false;
+    //     //     }
+    //     // })
 
-        //venuesList
-        // wx.request({
-        //     url: 'http://huanqiuxiaozhen.com/wemall/venues/venuesList',
-        //     method: 'GET',
-        //     data: {},
-        //     header: {
-        //         'Accept': 'application/json'
-        //     },
-        //     success: function(res) {
-        //         that.setData({
-        //             venuesItems: res.data.data
-        //         })
-        //         setTimeout(function () {
-        //             that.setData({
-        //                 loadingHidden: true
-        //             })
-        //         }, 1500)
-        //     }
-        // })
+    //   //productItem
+        
+    //     wx.request({
+    //       url: 'http://zms123.51vip.biz/wemall/venues/venuesList',
+    //         method: 'GET',
+    //         data: {},
+    //         header: {
+    //             'Accept': 'application/json'
+    //         },
+    //         success: function(res) {
+    //             that.setData({
+    //               productItem: data.data
+    //             })
+    //             setTimeout(function () {
+    //                 that.setData({
+    //                     loadingHidden: true
+    //                 })
+    //             }, 1500)
+    //         }
+    //     })
 
-        //choiceList
-        // wx.request({
-        //     url: 'http://huanqiuxiaozhen.com/wemall/goods/choiceList',
-        //     method: 'GET',
-        //     data: {},
-        //     header: {
-        //         'Accept': 'application/json'
-        //     },
-        //     success: function(res) {
-        //         that.setData({
-        //             choiceItems: res.data.data.dataList
-        //         })
-        //         setTimeout(function () {
-        //             that.setData({
-        //                 loadingHidden: true
-        //             })
-        //         }, 1500)
-        //     }
-        // })
+    //     choiceList
+    //     wx.request({
+    //         url: 'http://zms123.51vip.biz/wemall/goods/choiceList',
+    //         method: 'GET',
+    //         data: {},
+    //         header: {
+    //             'Accept': 'application/json'
+    //         },
+    //         success: function(res) {
+    //             that.setData({
+    //                 choiceItems: [1,2,3]
+    //             })
+    //             setTimeout(function () {
+    //                 that.setData({
+    //                     loadingHidden: true
+    //                 })
+    //             }, 1500)
+    //         }
+    //     })
 
-    }
+    // }
 })
